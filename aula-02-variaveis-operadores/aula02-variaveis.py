989994fa50680376d048feeb950a5617c21f165a
wendelprezende <wendelprezende@gmail.com>
2026-03-16 09:52:49 -0300

    aula 02 - variaveis

---
+++ aula-02-variaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,47 @@
+print ("Olá, mundo!")
+
+print (7 + 4)
+
+print("7" + "4") # CONCATENANDO STRINGS
+
+# Comentários em python utiliza a #
+"""
+    Comentários
+    de múltiplas
+    linhas
+"""
+
+nome = "Alexandre"
+idade = 26
+peso = 70.2
+
+print(nome, idade, peso)
+
+# INPUT - UTILIZADO PARA RECEBER DADOS DO USUÁRIO
+
+nome = input ("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
+
+# DESAFIO 01
+
+name = input ("Digite seu nome: ")
+
+print ("Seja bem vindo,", name)
+
+# DESAFIO 02
+
+dia = input("Digite o seu dia de nascimento: ")
+mes = input("Digite o seu mês de nascimento: ")
+ano = input("Digite o seu ano de nascimento: ")
+
+print (dia, mes, ano)
+
+# DESAFIO 03
+
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+
+print (num1 + num2)
